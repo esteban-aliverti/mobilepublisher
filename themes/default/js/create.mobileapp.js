d80cb753e2f7df809bb14f3fbd6ce1c20849748b
Chatura Dilan <dilan@ws02mobile.com>
2013-09-13 22:43:57 +0530

    publisher add mobile apps

--- themes/default/js/create.mobileapp.js
+++ themes/default/js/create.mobileapp.js
@@ -1,3 +1,77 @@
+var appMetaData = null;
+
+$('#application-tab a').click(function(e) {
+	e.preventDefault();
+	$(this).tab('show');
+});
+
+
+ 
+ 
+$('#txtOS').on("change",function() {
+	  if($('#txtOS').val() == 'webapp'){
+		  $('#control-webapp').show();
+	  }else{
+		  $('#control-webapp').hide();
+	  }
+});
+
+
+$('#txtMarket').on("change",function() {
+	  if($('#txtMarket').val() == 'Market'){
+		  $('#control-packagename').show();
+	  }else{
+		  $('#control-packagename').hide();
+	  }
+});
+
+
+$('#btn-create-asset').click(function(e) {
+	
+	var name = $("#txtName").val();
+	var description = $("#txtDescription").val();
+	var category = $("#txtCategory").val();
+	var recentChanges = $("#txtRecentChanges").val();
+	var banner = $("#txtbanner").val();
+	var screenShot1 = $("#txtScreenShot1").val();
+	var screenShot2 = $("#txtScreenShot2").val();
+	var screenShot3 = $("#txtScreenShot3").val();	
+	var iconfile = $("#txtIconfile").val();
+	var isMeetGudeLines = $("#chkMeetGudeLines").val();
+	
+	var params = {
+        name: name,
+        description: description,
+        category: category,
+        recentChanges: recentChanges,
+        banner: banner,
+        screenShot1: screenShot1,
+        screenShot2: screenShot2,
+        screenShot3: screenShot3,
+        iconfile: iconfile,
+        isMeetGudeLines: isMeetGudeLines,
+        url: "downloads/agent.apk",
+        provider: "wso2",
+        version: "1.0",
+        metadata : appMetaData	
+     };
+	
+	
+	$.ajax({
+      type: "POST",
+      url: "/publisher/api/apps",
+      contentType: "application/json",
+      data: JSON.stringify(params),
+      success: function () {
+        alert("Data Uploaded: ");
+      }
+    });
+});
+
+
+
+
+
 $(document).ready(function(){
 
 	$('#txtAppUpload').fileuploadFile({
